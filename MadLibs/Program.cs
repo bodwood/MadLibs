540d0a742f0cafe9b3e0a59dabc320139b73969f
bodwood <bodiewood@hotmail.com>
2022-12-12 12:30:26 -0800

    add initial project setup files

---
+++ MadLibs/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MadLibs
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args); //creates a web app
+
+      builder.Services.AddControllersWithViews();
+
+      WebApplication app = builder.Build(); //creates webapplication host app
+
+      app.UseHttpsRedirection();  //configure our host to redirect to HTTPS when web app is accessed via HTTP
+
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+
+      app.Run();
+    }
+  }
+}
